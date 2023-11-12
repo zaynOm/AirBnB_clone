cd23cfbfd72149f83ea99d328b25ae6f01ee9cb1
zaynOm <ouaziz0626@gmail.com>
2023-11-12 23:40:23 +0100

    adding comments

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
-""
+"Defines the FileStorage class"
 import json
 
 
 class FileStorage:
-    ""
+    """
+    serializes instances to a JSON file and deserializes JSON file to instances
+
+    Attributes:
+        __file_path (str): Path to the JSON file.
+        __objects (dict): Stores all objects.
+    """
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        ""
+        "returns the dictionary __objects"
         return self.__objects
 
     def new(self, obj):
-        ""
+        """adds a new object to __objects
+
+        Args:
+            obj (obj): The new object to add to __objects
+        """
         key = obj.__class__.__name__ + '.' + obj.id
         self.__objects[key] = obj
 
     def save(self):
-        ""
+        "serializes __objects to the JSON file"
         data = dict(self.__objects)
         for k, v in data.items():
             data[k] = v.to_dict()
@@ -27,7 +37,7 @@ class FileStorage:
             json.dump(data, f)
 
     def reload(self):
-        ""
+        "deserializes the JSON file to __objects"
         from models.base_model import BaseModel
         from models.user import User
         from models.state import State
