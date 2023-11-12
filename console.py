e4fc39ed490ae20aa68eec0fcb67dca06df412db
zaynOm <ouaziz0626@gmail.com>
2023-11-12 23:57:52 +0100

    console

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""
+"The entry point of the command interpreter"
 import cmd
 from models.base_model import BaseModel
 from models.user import User
@@ -12,8 +12,12 @@ from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    ""
+    """Command interpreter implementarion.
 
+    Attributs:
+        prompt (str): The prompt issued to solicit input.
+        cls_names (list): List containing class names.
+    """
     prompt = '(hbnb) '
     cls_names = ['BaseModel', 'User', 'State', 'City',
                  'Amenity', 'Place', 'Review']
@@ -23,15 +27,17 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_EOF(self, line):
-        "EOF command to exit the program"
+        "EOF (ctrl + D) command to exit the program"
         print()
         return True
 
     def emptyline(self):
+        "emptyline command to ignore 'ENTER' key."
         pass
 
     def do_create(self, line):
-        ""
+        "Creates a new instance of the given class."
+
         if self.validate_input(line):
             classes = {'BaseModel': BaseModel,
                        'User': User,
@@ -46,20 +52,20 @@ class HBNBCommand(cmd.Cmd):
             print(obj.id)
 
     def do_show(self, line):
-        ""
+        "Prints info of an instance based on the class name and id."
         if self.validate_input(line, True):
             args = line.split()
             print(storage.all()[args[0] + '.' + args[1]])
 
     def do_destroy(self, line):
-        ""
+        "Deletes an instance based on the class name and id."
         if self.validate_input(line, True):
             args = line.split()
             del storage.all()[args[0] + '.' + args[1]]
             storage.save()
 
     def do_all(self, line):
-        ""
+        "Prints all infos of all instances based or not on the class name."
         lenght = len(line.split())
         if lenght == 1 and line not in self.cls_names:
             print("** class doesn't exist **")
@@ -72,7 +78,7 @@ class HBNBCommand(cmd.Cmd):
             print(objs)
 
     def do_update(self, line):
-        ""
+        "Updates an instance based on the class name and id."
         if self.validate_input(line, True, True):
             args = line.split()
             obj = storage.all()[args[0] + '.' + args[1]]
@@ -80,6 +86,16 @@ class HBNBCommand(cmd.Cmd):
             obj.save()
 
     def validate_input(self, line, has_id=False, has_attr=False):
+        """Validates the input given by the user.
+
+        Args:
+            line (str): The user input.
+            has_id (bool): Indicates if the 'line' contains an id or not.
+            has_attr (bool): Indicates if the 'line' contains attributs or not.
+
+        Returns:
+            bool: True for valid input, False otherwise.
+        """
         args = line.split()
         lenght = len(args)
         if lenght == 0:
